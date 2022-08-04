5eb929e61dcce663e5bcf8074f7d1b40d0aaceab
AlvaroGV <alvaro.gasc@coverwallet.com>
2022-08-04 17:13:48 +0200

    feat(ms-477): update invoices endpoint

--- src/cwApi.ts
+++ src/cwApi.ts
@@ -834,6 +834,42 @@ export class Api<SecurityDataType extends unknown> extends HttpClient<SecurityDa
         format: "json",
         ...params,
       }),
+
+    /**
+     * @description Create a new invoice
+     *
+     * @tags Account, Invoices
+     * @name AddAccountDocumentInvoice
+     * @summary Create a new invoice
+     * @request POST:/accounts/{id}/invoices
+     * @secure
+     */
+    addAccountDocumentInvoice: (
+      id: string,
+      data: {
+        data: {
+          invoice: {
+            external_id: string;
+            account_id: string;
+            document_number: string;
+            document: string;
+            billing_date: string;
+            total: { amount: number; currency: string };
+          };
+          lines: { policy_id: string; transaction: { effective_date: string } }[];
+        };
+      },
+      params: RequestParams = {},
+    ) =>
+      this.request<{ status?: string }, { errors?: { source?: string; type: string; message: string }[] }>({
+        path: `/accounts/${id}/invoices`,
+        method: "POST",
+        body: data,
+        secure: true,
+        type: ContentType.Json,
+        format: "json",
+        ...params,
+      }),
   };
   policies = {
     /**
@@ -1202,42 +1238,6 @@ export class Api<SecurityDataType extends unknown> extends HttpClient<SecurityDa
         path: `/intermediaries`,
         method: "GET",
         secure: true,
-        format: "json",
-        ...params,
-      }),
-  };
-  invoices = {
-    /**
-     * @description Create a new invoice
-     *
-     * @tags Invoices
-     * @name AddAccountDocumentInvoice
-     * @summary Create a new invoice
-     * @request POST:/invoices
-     * @secure
-     */
-    addAccountDocumentInvoice: (
-      data: {
-        data: {
-          invoice: {
-            external_id: string;
-            account_id: string;
-            document_number: string;
-            document: string;
-            billing_date: string;
-            total: { amount: number; currency: string };
-          };
-          lines: { policy_id: string; transaction: { effective_date: string } }[];
-        };
-      },
-      params: RequestParams = {},
-    ) =>
-      this.request<{ status?: string }, { errors?: { source?: string; type: string; message: string }[] }>({
-        path: `/invoices`,
-        method: "POST",
-        body: data,
-        secure: true,
-        type: ContentType.Json,
         format: "json",
         ...params,
       }),
